05c3a1de7be91096b154aca19552e5d1d4d34c6f
adamprzywuski <adamprzywuski98@gmail.com>
2019-07-22 13:10:19 +0200

    Exchanging URL

--- app/src/main/java/com/example/mantis_displaying/MainActivity.java
+++ app/src/main/java/com/example/mantis_displaying/MainActivity.java
@@ -3,18 +3,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.jar.JarEntry;
-import android.os.StrictMode;
 import android.os.Bundle;
 import android.widget.*;
+
+import com.example.mantis_displaying.example.Example;
+import com.example.mantis_displaying.example.Field;
+
+import kotlin.jvm.Throws;
 import retrofit2.Call;
 import  retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.converter.gson.GsonConverterFactory;
-import android.os.NetworkOnMainThreadException;
-
-import android.widget.Toast;
-import android.view.*;
 import retrofit2.*;
 
 
@@ -38,23 +37,29 @@ public class MainActivity extends AppCompatActivity {
          .build();
         api api=retrofit.create(api.class);
 
-        Call<List<projects>> call = api.getData();
-        call.enqueue(new Callback<List<projects>>() {
+        Call<List<Example>> call = api.getData();
+            call.enqueue(new Callback<List<Example>>() {
             @Override
-            public void onResponse(Call<List<projects>> call, Response<List<projects>> response) {
+            public void onResponse(Call<List< Example>> call, Response<List<Example>> response) {
 
-                    List<projects> contributors = response.body();
-                    for (projects contributor : contributors) {
-                        String wynik="";
-                        wynik+=contributor.name + " (" + String.valueOf(contributor.getId()) + ")" + contributor.getDescirption();
-                        textt.setText(wynik);   }
+                   if (!response.isSuccessful()) {
+                         textt.setText("Code: " + response.code());
+                        return;
+            }
 
-
+                List<Example> posts = response.body();
+               for (Example project : posts) {
+                    String content = "";
+                    content += "ID: " + project.getId() + "\n";
+                    content += "Title: " + project.getSummary() + "\n";
+                    content += "Description: " + project.getDescription() + "\n";
+                    textt.append(content);
+                  }
             }
 
             @Override
-            public void onFailure(Call<List<projects>> call, Throwable t) {
-                    textt.setText("Failure");
+            public void onFailure(Call<List<Example>> call, Throwable t) {
+                    textt.setText(t.getMessage());
             }
         });
 
--- app/src/main/java/com/example/mantis_displaying/api.java
+++ app/src/main/java/com/example/mantis_displaying/api.java
@@ -1,23 +1,26 @@
 package com.example.mantis_displaying;
 
+import com.example.mantis_displaying.example.Example;
+
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
 
 import retrofit2.http.GET;
-import retrofit2.http.Header;
+
 import retrofit2.http.Headers;
 import retrofit2.http.Part;
-import retrofit2.http.Path;
+
+
 
 
 public interface api {
 
 
-    @Headers("Authorization:Xdoyk8Pi3lmLuKjpAF6bk6tDHeMfXk36 ")
-    @GET("/rest/issues?page_size=3&page=1")
-    Call<List<projects>> getData();
+    @Headers("Authorization:hV6kKJZA5VN-ZsfP2teVpxvw833bpLGh")
+    @GET("/api/rest/issues?page_size=10&page=1")
+    Call<List<Example>> getData();
 
 
 
