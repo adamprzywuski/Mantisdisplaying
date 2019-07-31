30b33172e4de80445cc5031914659caf975e3fc8
adamprzywuski <adamprzywuski98@gmail.com>
2019-07-31 12:04:37 +0200

    Hiding my token

--- app/src/main/java/com/example/mantis_displaying/api.java
+++ app/src/main/java/com/example/mantis_displaying/api.java
@@ -17,17 +17,13 @@ import retrofit2.http.Query;
 public interface api {
 //Interefence where are Http function
 
-    @Headers("Authorization:hV6kKJZA5VN-ZsfP2teVpxvw833bpLGh")
+    @Headers("Authorization:??WROTE_YOURS_TOKEN")
     @GET("/api/rest/issues?page_size=10&page=1")
     Call<Issues> getData();
 
+    
 
- //   @Headers("Authorization:hV6kKJZA5VN-ZsfP2teVpxvw833bpLGh")
-  //  @GET("/api/rest/issues?page_size=10&page=1")
-   // Call<win> getAllIssues();
-
-
-    @Headers({"Authorization:hV6kKJZA5VN-ZsfP2teVpxvw833bpLGh",
+    @Headers({"Authorization:??WROTE_YOURS_TOKEN",
             "Content-Type: application/json"})
    @POST("/api/rest/issues/{issue_id}/notes")
     Call<sending>sendNotes(
@@ -36,14 +32,14 @@ public interface api {
 
 
 
-    @Headers("Authorization:hV6kKJZA5VN-ZsfP2teVpxvw833bpLGh")
+    @Headers("Authorization:??WROTE_YOURS_TOKEN")
     @GET("/api/rest/issues/{issue_id}")
     Call<Issues> getIssues(
             @Path("issue_id") int id
     );
 
 
-    @Headers("Authorization:hV6kKJZA5VN-ZsfP2teVpxvw833bpLGh")
+    @Headers("Authorization:??WROTE_YOURS_TOKEN")
     @GET("/api/rest/issues?page=1")
     Call<Issues> getDatas(@Query("page_size") int id);
 
